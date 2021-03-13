89dd4909c83648d85e3c04f5187130f815830d49
Edgar <regalado.correo@gmail.com>
2021-03-12 22:56:26 -0800

    Add files via upload

---
+++ app_registrar.py
@@ -0,0 +1,18 @@
+
+class Registrar:
+    #clase motor de ejemplo
+    #una clase motor es la clase que tiene toda la funcionalidad logica
+    #tomando en cuenta solo datos y operaciones, nada grafico
+    def __init__(self):
+        self.mensaje = ""
+
+    def nombre(self, nombre):
+        self.mensaje = nombre
+    
+    def mostrar_nombre(self):
+        print("el nombre fue: {}".format(self.nombre))
+
+if __name__ == '__main__': #si se corre directamente en consola
+    reg = Registrar()
+    reg.nombre("Maradonio")
+    reg.mostrar_nombre()
